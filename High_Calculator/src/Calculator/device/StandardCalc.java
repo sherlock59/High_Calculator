01f27abc8c1076f39403a3c6633fffa93730d5a7
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-27 17:44:05 -0500

    added more fucntions

--- High_Calculator/src/Calculator/device/StandardCalc.java
+++ High_Calculator/src/Calculator/device/StandardCalc.java
@@ -1,10 +1,41 @@
+/*
+ * this class handles the memory function for the whole app 
+ * 
+ * @author Annaberdi Meredov
+ * @version 2.2
+ * @since 2022.04.27
+ */
 package Calculator.device;
 
-public class StandardCalc {
+public class StandardCalc extends Calculator {
+	
+	protected double memory = 0.0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+public  StandardCalc() {}
 
+/*
+ * this function sets the memory value to zero
+ */
+public void memoryReset() {
+	memory = 0.0;
+	System.out.println("Memory Reset: " + this.memory);
+}
+
+public void memoryAdd() {
+	System.out.print("Memory add: ");
+	this.memory += this.current;
+	System.out.println(this.memory);
+}
+
+public void memorySubtract() {
+	System.out.print("Memory Subtract: ");
+	this.memory = this.memory - this.current;
+	System.out.println(this.memory);
+}
+
+public void memoryRecall() {
+	System.out.println("Memory Recall:" + this.memory);
+}
+	
 }
