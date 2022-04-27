155dd09fd7775f74d25d40df37b4d1757243965c
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-27 00:15:18 -0500

    edited

--- High_Calculator/src/Calculator/device/AdvanceCalc.java
+++ High_Calculator/src/Calculator/device/AdvanceCalc.java
@@ -1,6 +1,12 @@
 package Calculator.device;
 
-public class AdvanceCalc {
+public class AdvanceCalc extends StandardCalc implements AdvanceMath {
+	
+	public AdvanceCalc() {
+}
+	
+	@Override
+	public void pow(double input)
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
