57801582a88810a9fd441f54495be401cf1209aa
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-27 17:10:52 -0500

    building an calc

--- High_Calculator/src/Calculator/device/AdvanceCalc.java
+++ High_Calculator/src/Calculator/device/AdvanceCalc.java
@@ -1,16 +1,26 @@
+/*
+ * This Calculator does necessary basic functions
+ * 
+ * @author Annaberdi Meredov
+ * @version 4.2
+ * @since 2022.04.27
+ */
+
 package Calculator.device;
 
 public class AdvanceCalc extends StandardCalc implements AdvanceMath {
 	
 	public AdvanceCalc() {
-}
+    }
 	
+	/*
+	 * Overrides power for method from the interface 
+	 */
 	@Override
-	public void pow(double input)
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void pow(double input) {
+		System.out.print(this.currentVal + "^" + input +  "is ");
+		this.current = (Math.pow(this.current,  input));
+		System.out.println(this.current);
 	}
 
 }
--- High_Calculator/src/Calculator/device/AdvanceMath.java
+++ High_Calculator/src/Calculator/device/AdvanceMath.java
@@ -1,5 +1,23 @@
+/*
+ * Advanced Math interface does square root and power method 
+ * 
+ * @author Annaberdi Meredov
+ * @version 4.2
+ * @since 2022.04.27
+ */
+
 package Calculator.device;
 
 public interface AdvanceMath {
-
+	
+	
+	/*
+	 * power of method from parent advanced Calc class
+	 */
+	void pow(double inpout);
+	
+	/*
+	 * Square root of method from parent advanced Calc class
+	 */
+	void sqrt();
 }
