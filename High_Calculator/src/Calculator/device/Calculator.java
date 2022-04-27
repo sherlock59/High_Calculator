8f38963c758bbde0124f4a5f53e2a8dadcdd6cbd
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-27 17:30:30 -0500

    added more methods

--- High_Calculator/src/Calculator/device/Calculator.java
+++ High_Calculator/src/Calculator/device/Calculator.java
@@ -1,10 +1,38 @@
+/*
+ * This class builds the functions such as +,-,/, and *
+ * 
+ * @author Annaberdi Meredov
+ * @version 2.2
+ * @since 2022.04.27
+ */
 package Calculator.device;
 
-public class Calculator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public abstract class Calculator implements BasicMath {
+	
+	protected double current = 0.0;
+	
+	public Calculator() {}
+	
+	public void clear() {
+		current = 0.0;
+		System.out.println("C :" + this.current);
+	}
+	
+	/*
+	 * Override methods below for +,-,/, and * 
+	 */
+	@Override
+	public void divide(double input) {
+		System.out.print(this.current + " * " + input + "=");
+		this.current = this.current * input;
+		System.out.println(this.current);
+	}
+	
+	@Override
+	public void multiply(double input) {
+		System.out.print(this.current + "*" + input + "=");
+		this.current = this.current * input;
+		System.out.println(this.current);
 	}
 
 }
