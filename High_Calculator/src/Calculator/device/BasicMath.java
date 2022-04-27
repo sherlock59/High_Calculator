4fb07e19acf2b8159707c928dc7f19442f571d3d
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-27 17:16:36 -0500

    implementing things into calculator app

--- High_Calculator/src/Calculator/device/BasicMath.java
+++ High_Calculator/src/Calculator/device/BasicMath.java
@@ -1,5 +1,24 @@
+/*
+ * This Basic math function forces 
+ * the calculator implement the add, subtract, 
+ * multiply, and divide functions
+ * 
+ * @author Annaberdi Meredov
+ * @version 2.2
+ * @since 2022.04.27
+ */
 package Calculator.device;
 
 public interface BasicMath {
-
+	
+	/*
+	 * below presented implementation to the calculator 
+	 */
+	void add(double input);
+	
+	void subtract(double input);
+	
+	void multiply(double input);
+	
+	void divide(double input);
 }
