71ff8aef99450fdb5943ab10b23dd806d7f3fef8
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-28 11:52:21 -0500

    edited and added more things to main app

--- High_Calculator/src/Calculator/app/MainApp.java
+++ High_Calculator/src/Calculator/app/MainApp.java
@@ -1,10 +1,34 @@
+/*
+ * This main tests the whole functions
+ * 
+ * @author Annaberdi Meredov
+ * @version 2.2
+ * @since 2022.04.27
+ */
+
 package Calculator.app;
+
+import Calculator.device.AdvanceCalc;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AdvanceCalc advanceClc = new AdvanceCalc();
+		
+		/*
+		 * Test conduction process for functions below
+		 */
+		advanceClc.add(10);
+		advanceClc.subtract(2);
+		advanceClc.memoryAdd();
+		advanceClc.multiply(10);
+		advanceClc.memorySubtract();
+		advanceClc.divide(2);
+		advanceClc.memoryRecall();
+		advanceClc.memoryReset();
+		advanceClc.pow(3);
+		advanceClc.sqrt();
+		advanceClc.clear();
 	}
 
 }
